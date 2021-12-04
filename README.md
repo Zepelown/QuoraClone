7a3da390254ee019960771921d77b3deab61e509
Zepelown <dbswjdqhd12@gmail.com>
2021-12-04 14:37:43 +0000

    Edit Navbar

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # QuoraClone
 [참고강좌](https://www.youtube.com/watch?v=j3-4QTcAcNk)
-14:28분
-
-git test
+21:03분
--- quora-clone/src/Navbar.js
+++ quora-clone/src/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
+
 import {
     AssignmentIndOutlined,
     BorderAllRounded, ExpandMore,
@@ -8,32 +9,34 @@ import {
     PeopleAltOutlined, Search
 } from "@material-ui/icons";
 
-function Navbar(props) {
-	return (
+function Navbar() {
+	return(
 		<div className="navbar">
-			<div className="qHeader_logo">
-				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Quora_logo_2015.svg/2880px-Quora_logo_2015.svg.png" alt=""/>
-				<div className="qHeader_icons">
-					<div className="qHeader_icon">
-						<Home/>
-					</div>
-					<div className="qHeader_icon">
-						<BorderAllRounded/>
-					</div>
-					<div className="qHeader_icon">
-						<AssignmentIndOutlined/>
-					</div>					
-					<div className="qHeader_icon">
-						<PeopleAltOutlined/>
-					</div>
-					<div className="qHeader_icon">
-						<NotificationsOutlined/>
-					</div>					
+		
+		<div className="qHeader_logo">
+			<img 				src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Quora_logo_2015.svg/2880px-Quora_logo_2015.svg.png" 
+			alt = ""/>
+		</div>
+			<div className="qHeader_icons">
+				<div className="qHeader_icon">
+					<Home/>
+				</div>
+				<div className="qHeader_icon">
+					<BorderAllRounded/>
+				</div>
+				<div className="qHeader_icon">
+					<AssignmentIndOutlined/>
+				</div>
+				<div className="qHeader_icon">
+					<PeopleAltOutlined/>
+				</div>
+				<div className="qHeader_icon">
+					<NotificationsOutlined/>
 				</div>
 			</div>
-		
+			
 		</div>
-	);
+	)
 }
 
 export default Navbar;
